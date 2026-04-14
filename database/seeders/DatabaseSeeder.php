063c671e71205717476272d460cdd35ef2e2e629
myZeez <budiaat25@gmail.com>
2026-04-14 16:05:06 +0700

    Use DiagnosisChatDataSeeder instead of duplicate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            DeviceMasterDataSeeder::class,
+            DiagnosisChatDataSeeder::class,
             ExpertSystemSeeder::class,
             AdminUserSeeder::class,
         ]);
